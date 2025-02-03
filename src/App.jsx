d4cb069cd98de09f5eefda3b8c4517aaa1a5c4d9
VladShev74 <vladadshev@gmail.com>
2025-02-03 21:52:56 +0100

    Added core components and updated css files + added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+import { Link, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import CreateQuiz from './components/CreateQuiz'
+import QuizPage from './components/QuizPage'
 import './App.css'
 
 const apiUrl = import.meta.env.VITE_API_URL
 console.log('API URL:', apiUrl)
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app">
+        <header className="header">
+          <nav className="navbar">
+            <Link to="/" className="logo">
+              Quizzer
+            </Link>
+            <div className="nav-links">
+              <Link to="/quiz-page">
+                <button className="add-quiz-button">Go to Quiz Page</button>
+              </Link>
+              <Link to="/quiz-create">
+                <button className="add-quiz-button">Create a Quiz</button>
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/quiz-page" element={<QuizPage />} />
+            <Route path="/quiz-create" element={<CreateQuiz />} />
+          </Routes>
+        </main>
+        <footer className="footer">
+          <p>&copy; 2025 Quizzer. All rights reserved.</p>
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
